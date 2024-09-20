833c2ba84addce3692a4c9af7b2301270eadcd3d
elliotallen1 <allened@hendrix.edu>
2024-09-20 16:38:03 -0500

    Created Juice and Shop pages
    Added the widgets. They are only Text right now

---
+++ juice_drinker/lib/widgets/shop_page_widget.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class ShopPage extends StatelessWidget {
+  const ShopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Shop Page'),
+    );
+  }
+}
