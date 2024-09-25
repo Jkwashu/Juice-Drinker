828b5c37953a1b3e0a7f4aa1af1a81174d69f067
Jxxl3 <SmithJJ3@hendrix.edu>
2024-09-25 01:15:25 -0500

    Added yaw math
    Using magnetometer and accelerometer, you can calculate the orientation, specifically the yaw, of the phone

--- juice_drinker/lib/pages/juice_page.dart
+++ juice_drinker/lib/pages/juice_page.dart
@@ -1,19 +1,106 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:juice_drinker/widgets/juice_widget.dart';
 import 'package:juice_drinker/objects/juice.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
-class JuicePage extends StatelessWidget {
+class JuicePage extends StatefulWidget {
   final Juice juice;
-
   const JuicePage({super.key, required this.juice});
 
   @override
-  Widget build(BuildContext context) {
-    double drinkHeight = 1.0;  // NEEDS TO BE BETWEEN 0 AND 1
+  _JuicePageState createState() => _JuicePageState();
+}
 
+class _JuicePageState extends State<JuicePage> {
+  double drinkHeight = 1.0; 
+
+  StreamSubscription<MagnetometerEvent>? _magnetometerSubscription;
+  StreamSubscription<AccelerometerEvent>? _accelerometerSubscription;
+
+  // Sensor readings
+  MagnetometerEvent? _magnetometerEvent;
+  AccelerometerEvent? _accelerometerEvent;
+
+  double _yawDegrees = 0.0; // Yaw angle in degrees
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Subscribe to magnetometer events
+    _magnetometerSubscription = magnetometerEvents.listen(
+      (MagnetometerEvent event) {
+        setState(() {
+          _magnetometerEvent = event;
+          _updateYaw(); // Update yaw when new magnetometer data is available
+          print(_yawDegrees);
+        });
+      },
+    );
+
+    // Subscribe to accelerometer events
+    _accelerometerSubscription = accelerometerEvents.listen(
+      (AccelerometerEvent event) {
+        setState(() {
+          _accelerometerEvent = event;
+          _updateYaw(); // Update yaw when new accelerometer data is available
+        });
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _magnetometerSubscription?.cancel();
+    _accelerometerSubscription?.cancel();
+    super.dispose();
+  }
+
+  // Method to update yaw
+void _updateYaw() {
+  if (_magnetometerEvent == null || _accelerometerEvent == null) {
+    return; // Wait until both sensor readings are available
+  }
+
+  // Get accelerometer readings
+  double accelX = _accelerometerEvent!.x;
+  double accelY = _accelerometerEvent!.y;
+  double accelZ = _accelerometerEvent!.z;
+
+  // Compute roll (phi) and pitch (theta)
+  double phi = atan2(accelY, accelZ);
+  double theta = atan2(-accelX, sqrt(accelY * accelY + accelZ * accelZ));
+
+  // Get magnetometer readings
+  double magX = _magnetometerEvent!.x;
+  double magY = _magnetometerEvent!.y;
+  double magZ = _magnetometerEvent!.z;
+
+  // Compensate magnetometer readings
+  double magXh = magX * cos(theta) + magZ * sin(theta);
+  double magYh = magX * sin(phi) * sin(theta) + magY * cos(phi) - magZ * sin(phi) * cos(theta);
+
+  // Compute yaw (psi)
+  double psi = atan2(magYh, magXh);
+
+  // Convert to degrees and adjust to standard compass directions
+  double yawDegrees = (psi * 180 / pi + 90) % 360;
+  
+  // Ensure the result is between 0 and 360
+  if (yawDegrees < 0) yawDegrees += 360;
+
+  setState(() {
+    _yawDegrees = yawDegrees;
+  });
+}
+
+  @override
+  Widget build(BuildContext context) {
     return Center(
       child: JuiceWidget(
-        juice: juice,
+        juice: widget.juice,
         height: drinkHeight,
       ),
     );
