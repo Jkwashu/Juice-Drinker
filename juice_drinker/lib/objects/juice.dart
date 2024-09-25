f833dd0893c21b159759f6677a29d8ce69f0b197
Joseph Washum <Jkwashu@gmail.com>
2024-09-24 22:41:40 -0500

    Juice shop framework up
    Added new parameters for juice and passed them through main, additionally fixed / added purchase logic for juice in shop.

--- juice_drinker/lib/objects/juice.dart
+++ juice_drinker/lib/objects/juice.dart
@@ -4,6 +4,12 @@ class Juice {
   final Color color;
   final String name;
   final int price;
+  bool purchased;
 
-  Juice({required this.color, required this.name, required this.price});
+  Juice({
+    required this.color,
+    required this.name,
+    required this.price,
+    this.purchased = false,
+  });
 }
--- juice_drinker/lib/pages/shop_page.dart
+++ juice_drinker/lib/pages/shop_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:juice_drinker/objects/juice.dart';
 
-class ShopPage extends StatelessWidget {
+class ShopPage extends StatefulWidget {
   final Function(Juice) onJuiceUpdate;
   final int coins;
 
@@ -14,13 +14,34 @@ class ShopPage extends StatelessWidget {
 
 class _ShopPageState extends State<ShopPage> {
   int _currentIndex = 0;
-  List<Juice> _juices = [
+  final List<Juice> _juices = [
     Juice(color: Colors.orange, name: 'Orange Juice', price: 20),
     Juice(color: Colors.purple, name: 'Grape Juice', price: 20),
     Juice(color: Colors.red, name: 'Cranberry Juice', price: 20),
     Juice(color: Colors.blue, name: 'Blueberry Juice', price: 20),
     Juice(color: Colors.green, name: 'Grapefruit Juice', price: 20),
   ];
+
+  void _purchaseJuice() {
+    if (widget.coins >= _juices[_currentIndex].price &&
+        !_juices[_currentIndex].purchased) {
+      setState(() {
+        _juices[_currentIndex].purchased = true;
+        widget.onJuiceUpdate(_juices[_currentIndex]);
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Not enough coins or already purchased!')),
+      );
+    }
+  }
+
+  void _equipJuice() {
+    widget.onJuiceUpdate(_juices[_currentIndex]);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('${_juices[_currentIndex].name} equipped!')),
+    );
+  }
 
   void _nextJuice() {
     setState(() {
@@ -81,10 +102,11 @@ class _ShopPageState extends State<ShopPage> {
           ),
           const SizedBox(height: 20),
           ElevatedButton(
-            onPressed: () {
-              // TODO: Implement purchase logic
-            },
-            child: Text('Buy for ${_juices[_currentIndex].price} coins'),
+            onPressed:
+                _juices[_currentIndex].purchased ? _equipJuice : _purchaseJuice,
+            child: Text(_juices[_currentIndex].purchased
+                ? 'Equip'
+                : 'Buy for ${_juices[_currentIndex].price} coins'),
           ),
         ],
       ),
