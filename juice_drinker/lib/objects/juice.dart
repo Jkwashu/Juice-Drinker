620e60515e5a95fee117b7a450c53ed69f3ddac9
Htjackson8506 <104510246+Htjackson8506@users.noreply.github.com>
2024-10-28 12:30:05 -0700

    Flutter pub get, added fizz class and boolean

--- juice_drinker/lib/objects/juice.dart
+++ juice_drinker/lib/objects/juice.dart
@@ -4,12 +4,14 @@ class Juice {
   final Color color;
   final String name;
   final int price;
+  final bool fizzy;
   bool purchased;
 
   Juice({
     required this.color,
     required this.name,
     required this.price,
+    required this.fizzy,
     this.purchased = false,
   });
 }
